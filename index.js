33030d9517b7574da4979f3eb7b1819042d198df
Ritik Thakur <rk7495933@gmail.com>
2025-08-12 23:32:26 +0530

    updated

--- index.js
+++ index.js
@@ -223,8 +223,8 @@ mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("DB connected!");
-    app.listen(process.env.PORT || 3002, () => {
-      console.log(`🚀 Server running on port ${process.env.PORT || 3002}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
